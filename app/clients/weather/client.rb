c5be1622c9d8cb10c914908dfe5f745176e7f8cd
Carlos Rodrigues <carlos.all.mail@gmail.com>
2023-10-16 16:52:44 -0600

    Gets the forecast url

--- app/clients/weather/client.rb
+++ app/clients/weather/client.rb
@@ -7,5 +7,17 @@ module Weather
       # API requires a maximum precision of 4 decimal places
       "#{WEATHER_API_URL}/#{latitude.truncate(MAX_PRECISION)},#{longitude.truncate(MAX_PRECISION)}"
     end
+
+    def get_forecast_url(latitude, longitude)
+      response = Faraday.get(make_weather_url(latitude, longitude))
+      if response.status == 200
+        JSON.parse(response.body).dig("properties", "forecast")
+      else
+        raise WeatherApiError.new(JSON.parse(response.body).dig("detail"))
+      end
+    end
+  end
+
+  class WeatherApiError < StandardError
   end
 end
--- spec/clients/weather/client_spec.rb
+++ spec/clients/weather/client_spec.rb
@@ -19,8 +19,47 @@ RSpec.describe Weather::Client do
   end
 
   describe "#get_forecast_url" do
-    it "calls the weather API"
-    it "returns the forecast url"
+    let(:forecast_url) { "https://api.weather.gov/gridpoints/LWX/97,71/forecast" }
+    let(:points_json) do
+      {
+        "properties"=> {
+          "@id"=> forecast_url,
+          "forecast"=>"https://api.weather.gov/gridpoints/LWX/97,71/forecast",
+          "forecastHourly"=>"https://api.weather.gov/gridpoints/LWX/97,71/forecast/hourly",
+          "forecastGridData"=>"https://api.weather.gov/gridpoints/LWX/97,71",
+        }
+      }.to_json
+    end
+
+    context "for a successful call" do
+      before(:each) do
+        stub_request(:get, /api\.weather\.gov\/points/)
+          .to_return(status: 200, body: points_json, headers: {})
+      end
+
+      it "calls the weather API" do
+        subject.get_forecast_url(latitude, longitude)
+        expect(a_request(:get, /api\.weather\.gov\/points/)).to have_been_made.once
+      end
+
+      it "returns the forecast url" do
+        expect(subject.get_forecast_url(latitude, longitude)).to eq(forecast_url)
+      end
+    end
+
+    context "for a unsuccessful call" do
+      let(:call_body) { { detail: "Unsuccessful" }.to_json }
+
+      before(:each) do
+        stub_request(:get, /api\.weather\.gov\/points/)
+          .to_return(status: 301, body: call_body, headers: {})
+      end
+
+      it "throws an exception" do
+        expect{ subject.get_forecast_url(latitude, longitude) }
+          .to raise_error(Weather::WeatherApiError)
+      end
+    end
   end
 
   describe "#get_forecast" do
